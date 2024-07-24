df644cbe300bc32a38e4fa576482a872774caaa2
Atul Ahire <atulahire9607@gmail.com>
2024-07-24 14:36:42 +0530

    trainer updates

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,39 @@
 import { createStore } from 'redux';
 
-//make a cardReducer function to use counting application
 
-const counterReducer = (state ={counter:0}, action)=>{
-        if(action.type === 'IncrementBy5'){
-            return {
-                counter : state.counter + 5
-            }
-        } 
+const initialState={ counter: 0, showCounter:true};
 
-        if(action.type === 'DecrementBy5'){
-            return {
-                counter : state.counter - 5
-            }
-        }
-        return state;
+const counterReducer = (state = initialState, action) => {
+  if (action.type === 'increment') {
+    state.counter++;
+    
+    return {
+      counter: state.counter + 1,
+      showCounter:state.showCounter
+    };
+  } 
+
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter + action.amount,
+      showCounter:state.showCounter
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+      showCounter:state.showCounter
+    };
+  }
+if(action.type==='toggle')
+{
+    return{
+        showCounter:!state.showCounter,
+        counter:state.counter
+    }
+}
+  return state;
 };
 
 const store = createStore(counterReducer);
